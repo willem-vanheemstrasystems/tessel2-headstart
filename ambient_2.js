5b680c5578cf294a75deefe4a481b1dece7b0912
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-27 17:26:18 +0100

    Added

---
+++ ambient_2.js
@@ -0,0 +1,26 @@
+/*********************************************
+This ambient module example console.logs
+ambient light and sound levels and whenever a
+specified light or sound level trigger is met.
+*********************************************/
+
+var tessel = require('tessel');
+var ambientlib = require('ambient-attx4');
+
+var ambient = ambientlib.use(tessel.port['A']);
+
+ambient.on('ready', function () {
+ // Get points of light and sound data.
+  setInterval( function () {
+    ambient.getLightLevel( function(err, ldata) {
+      if (err) throw err;
+      ambient.getSoundLevel( function(err, sdata) {
+        if (err) throw err;
+        console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
+    });
+  })}, 500); // The readings will happen every .5 seconds unless the trigger is hit
+});
+
+ambient.on('error', function (err) {
+  console.log(err)
+});
